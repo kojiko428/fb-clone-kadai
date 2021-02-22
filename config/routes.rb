5fba9434824e82c7d67b2c64f5433b06651d4059
koji_yamamoto <koji_ko428@yahoo.co.jp>
2021-02-22 07:31:32 +0000

    kadai start(Blog機能実装済)

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  get '/blogs', to: 'blogs#index'
+  get '/stocks', to: 'stocks#index'
+
+    resources :blogs do
+
+    collection do
+      post :confirm
+    end
+  end
+
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
