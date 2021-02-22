5c8103a3322403e3f4e65200a4fd4a1e9a7a46ed
koji_yamamoto <koji_ko428@yahoo.co.jp>
2021-02-22 07:55:57 +0000

    Bootstrapの導入

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
     end
   end
 
-
-
+  root to: 'tops#index'  
+    resources :blogs
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
