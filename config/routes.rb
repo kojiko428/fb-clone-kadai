8da724f94830afd9288a0382b45a207fe3b4f611
koji_yamamoto <koji_ko428@yahoo.co.jp>
2021-02-24 07:28:49 +0000

    ログイン状態での処理の実装・ログイン中のユーザを取得

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get '/blogs', to: 'blogs#index'
   get '/stocks', to: 'stocks#index'
 
@@ -14,8 +15,12 @@ Rails.application.routes.draw do
   root to: 'tops#index'
     resources :blogs
 
-# ログインシステム
-  resources :users, only: [:new, :create]
+# ユーザー新規登録
+  resources :users, only: [:new, :create, :show]
+
+# ログインシステム・ログアウトシステム
+ resources :sessions, only: [:new, :create, :destroy]
+ resources :users, only: [:new, :create, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
